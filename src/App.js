ebb1bfe24798f2d3fa4b87052da6d8ff24d965aa
redpanda-cat <leungs1@mskcc.org>
2021-11-30 12:23:24 -0800

    Add Sankey

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./data.json";
+import { Sankey } from "@shahlab/planetarium";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Sankey
+      data={data}
+      width={800}
+      height={700}
+      subsetParam="cell_type"
+      cloneParam="clone_id"
+      timepointOrder={["Pre", "Post"]}
+      timepointParam="treatment"
+    />
   );
 }
 
